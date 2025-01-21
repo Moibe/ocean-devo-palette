f3fdf7b36b5373df7a1a8b195d3453fafc02d786
Moibe <moi_estrello@hotmail.com>
2025-01-21 17:11:11 -0600

    New api name acuarela

--- globales.py
+++ globales.py
@@ -1,21 +1,16 @@
 import gradio as gr
 
 #MAIN
-version = "0.0.3"
+version = "0.1.2"
 env = "dev"
 aplicacion = "palette-dev"
 
 seleccion_api = "eligeGratisOCosto" #eligeGratisOCosto , eligeAOB o eligeGratisOCosto
 max_size = 20
-#Quota o Costo
-api_zero = ("Moibe/image-blend", "quota")
-api_cost = ("Moibe/image-blend", "costo")
-#A o B
-api_a = ("Moibe/image-blend", "gratis")
-api_b = ("Moibe/image-blend", "gratis")
+
 #Gratis o Costo
-api_gratis = ("Moibe/deteccion-colores", "gratis")
-api_costo = ("Moibe/deteccion-colores", "costo")
+api_gratis = ("Moibe/acuarela", "gratis")
+api_costo = ("Moibe/acuarela", "costo")
 
 interface_api_name = "/gradio_interface" #El endpoint al que llamará client.
 
